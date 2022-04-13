eedd86e91527d00b2c8d980abc113f0ec07f2338
Zeitlin2115 <103568419+Zeitlin2115@users.noreply.github.com>
2022-04-13 19:59:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projekt1</title>
+</head>
+
+    <body>
+
+    <header>
+    <h1>STRONA GŁOWNA</h1>
+    </header>
+
+
+    <div id="l">
+    <h2> Linki:</h2><br>
+    <h3><a href="about_me.php">O mnie</a></h3> <br>
+    <h3><a href="contact.php">Kontakt</a></h3> <br>
+    </div>
+
+    <div id="p">
+    <h2>PRO8L3M - początki</h2>
+
+    PRO8L3M – polski zespół hip-hopowy, założony w 2013 w Warszawie przez producenta muzycznego DJ-a Piotra „DJ Steeza” Szulca i rapera Oskara Tuszyńskiego.<br><br>
+
+    Ukazał się wtedy minialbum "C30-C39", materiał undegroundowy, kłaniający się bujającej starej szkole warszawskiego rapu z lat 90., ale też brzmiący nieco futurystycznie dzięki syntezatorom. <br>
+    Promowały go wideoklipy do utworów "Opowieść o tobie" i "Tori Black" oraz napisy na śródmiejskich murach "Pro8l3m łaki" (od angielskiego "wack MC’s", czyli gówniani raperzy).<br><br>
+
+    Wiosną 2014 Pro8l3m wydał może swój najbardziej niespodziwany utwór – "Krzyk" inspirowany historią Ryszarda Siwca. Mężczyzna, były żołnierz AK, w proteście przeciw najazdowi wojsk Układu Warszawskiego na Czechosłowację w 1968 roku dokonał samospalenia podczas dożynek na Stadionie Dziesięciolecia w Warszawie.<br>
+    <br>
+    We wrześniu 2014 roku ukazał się przełomowy mixtape "Art Brut", który przysporzył Pro8l3mowi wielkiej popularności. Był to album koncepcyjny: utwory w ogromnej większości oparto na samplach z polskiej muzyki rozrywkowej z lat 70. i 80., m.in. piosenek Haliny Frąckowiak, Krystyny Prońko, zespołu Dwa Plus Jeden. Do tego dialogi z gangsterskich filmów "Wielki Szu" i "Sztos". Płytę promowały utwory "Stówa" (do którego znacznie później powstał koncertowy wideoklip w technologii 360 VR), "TEB 200-1", "Jakby kończył się". Wszystko w jednym kawałku znalazło się na YouTube – twórcom zależało na tym, by słuchacze poznawali materiał ciągiem, od A do Z.
+    <br>
+    <br>
+    Po wydaniu "Art Brut" duet zdobył nagrodę Nocnego Marka dla artysty roku przyznawaną przez czytelników "Aktivist".
+    <br>
+    <br>
+
+      <img src="aaa.jpg" width="50%" > </div>
+
+    <?php 
+    include "components/footer.php";
+    ?>
+
+    </body>
+</html>
